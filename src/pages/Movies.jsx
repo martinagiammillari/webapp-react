b861f86c850412422bbdced68541301e5fffc585
martinagiammillari <martigiammi@gmail.com>
2026-01-20 16:59:03 +0100

    feat: milestone 3 done

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,19 @@
+import axios from "axios";
 import { useEffect } from "react";
 
 export default function Movies() {
   useEffect(()=> {
     console.log("chiamo api")
-  })
+    axios
+    .get("http://localhost:3600/api/movies")
+    .then((resp)=>{
+      console.log(resp.data.results);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  
+  }, []);
   return (
     <div className="container mt-4">
       <h1>Movies</h1>
