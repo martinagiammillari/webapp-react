3082c9c5ac5e72fd83e25a7d05ef1b85da5d15d5
martinagiammillari <martigiammi@gmail.com>
2026-01-21 14:25:05 +0100

    feat: card with image done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,32 @@
 export default function MovieCard({ movie }) {
   return (
     <div className="card h-100">
-      <div className="card-body">
+      {/* Immagine */}
+      <img
+        src={`http://localhost:3600/images/${movie.image}`}
+        className="card-img-top"
+        alt={movie.title}
+      />
+
+      <div className="card-body d-flex flex-column">
+        {/* Titolo */}
         <h5 className="card-title">{movie.title}</h5>
 
+        {/* Regista */}
+        <h6 className="card-subtitle mb-2 text-muted">
+          Directed by {movie.director}
+        </h6>
+
+        {/* Abstract */}
         <p className="card-text">
-          Year: {movie.release_year}
+          {movie.abstract}
         </p>
 
-        <p className="card-text">
-          Genre: {movie.genre}
+        {/* Info extra */}
+        <p className="card-text mt-auto">
+          <small className="text-muted">
+            Year: {movie.release_year} • Genre: {movie.genre}
+          </small>
         </p>
       </div>
     </div>
